60658a0cce5f35be2ca5fbf0118e4453931b5a7d
Aiman Sahharon <aimansahharon44@gmail.com>
2024-01-07 18:57:31 +0800

    v1.0: create project and add SoftDeletes

---
+++ app/Http/Controllers/RequestProjectController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RequestProject;
+use Illuminate\Http\Request;
+
+class RequestProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RequestProject $requestProject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RequestProject $requestProject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RequestProject $requestProject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RequestProject $requestProject)
+    {
+        //
+    }
+}
